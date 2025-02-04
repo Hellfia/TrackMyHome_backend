1475450ef0f329830d58cf42a8fc94b5aca28ebb
Steeve Bonenfant <bonenfantstee@gmail.com>
2025-02-04 09:04:15 +0100

    setup backend

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,6 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
-
-
-
-
-
-  
 });
 
 module.exports = router;
