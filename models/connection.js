b817ddbf45f3139bd3bac7afa4d0b97c4e6b25d6
Steeve Bonenfant <bonenfantstee@gmail.com>
2025-02-07 17:04:55 +0100

    add mongodb connexion

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
 
-const connectionString = "";
+const user = process.env.USERNAME;
+const password = process.env.PASSWORD;
+
+const connectionString = `mongodb+srv://${user}:${password}@myfirstdatabase.nc8ad.mongodb.net/trackmyhome`;
 
 mongoose
   .connect(connectionString, { connectTimeoutMS: 2000 })
